4fdca41aebc7fb276cca732d3f3dad3b84145fa6
Christian Wiedemann <christian.wiedemann@key-tec.de>
2022-02-09 21:21:23 +0100

    feat(link): Add option to prefill link urls of button component

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -25,20 +25,25 @@ class ConfigForm extends FormBase {
     $form['dist_path'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Dist path'),
-      '#description' => $this->t('A local file system path to your dist/app-drupal directory.'),
+      '#description' => $this->t(
+        'A local file system path to your dist/app-drupal directory.'
+      ),
       '#maxlength' => 128,
       '#size' => 64,
       '#default_value' => $config->get('dist_path'),
-      '#weight' => '0',
+      '#weight' => 10,
     ];
     $form['only_own_layout'] = [
+      '#weight' => 20,
       '#type' => 'checkbox',
       '#title' => $this->t('Use only Wingsuit patterns.'),
-      '#description' => $this->t('Check this to hide all other layouts in layout builder.'),
+      '#description' => $this->t(
+        'Check this to hide all other layouts in layout builder.'
+      ),
       '#default_value' => $config->get('only_own_layout'),
-      '#weight' => '0',
     ];
     $form['submit'] = [
+      '#weight' => 30,
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
     ];
@@ -57,10 +62,17 @@ class ConfigForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->configFactory()->getEditable('wingsuit_companion.config')
-      ->set('dist_path', $form_state->getValue('dist_path'))
-      ->set('only_own_layout', $form_state->getValue('only_own_layout'))
-      ->save();
+    $config = $this->configFactory()->getEditable('wingsuit_companion.config');
+    foreach ($form_state->getValues() as $key => $value) {
+      if (!in_array(
+        $key,
+        ['submit', 'form_build_id', 'form_token', 'form_id', 'op']
+      )) {
+        $config->set($key, $value);
+      }
+
+    }
+    $config->save();
     $this->messenger()->addMessage('Wingsuit settings stored successfully.');
   }
 
